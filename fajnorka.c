b7e5c03244f578fad426e74b53a8515ff3754675
LukacTomas <lukactv@gmail.com>
2020-08-05 21:24:53 +0200

    Update on speed -memcpy vs strcpy

--- fajnorka.c
+++ fajnorka.c
@@ -1,16 +1,16 @@
 /**
  * Fajnorka Library for C
  * https://github.com/LukacTomas/Fajnorka-Library.git
- * 
- * Library inspired by cs50 library 
- * and Learn the C hard way book by Zed Shaw 
- * 
+ *
+ * Library inspired by cs50 library
+ * and Learn the C hard way book by Zed Shaw
+ *
  * Author: Lukac Tomas
  * email: lukactv@gmail.com
- * 
+ *
  *  Copyright (c) 2020
  *  All rights reserved
- * 
+ *
  * BSD 3-Clause License
  * http://www.opensource.org/licenses/BSD-3-Clause
  *
@@ -42,11 +42,11 @@
 
 #include "fajnorka.h"
 
-/**
- * string_holder_init()
- * 
- * inicializacia parametrov string_holdera
- * */
+ /**
+  * string_holder_init()
+  *
+  * inicializacia parametrov string_holdera
+  * */
 #undef string_holder_init
 void string_holder_init()
 {
@@ -64,8 +64,8 @@ void string_holder_init()
 
 /**
  * string_holder_resize()
- * 
- * Ak treba zvacit pole string array v string_holderi 
+ *
+ * Ak treba zvacit pole string array v string_holderi
  * */
 #undef string_holder_resize
 void string_holder_resize()
@@ -82,7 +82,7 @@ void string_holder_resize()
 
 /**
  * string_holder_destroy()
- * 
+ *
  * Uvolnenie pamati celeho string_holdera
  * */
 #undef string_holder_destroy
@@ -99,7 +99,7 @@ void string_holder_destroy()
 
 /**
  * string_holder_free_last()
- * 
+ *
  * Uvolnenie pamati posledneho miesta v pameti v string array
  * v string_holderi (Ak nepotrebujeme drzat string v pamati dlho)
  * */
@@ -111,7 +111,7 @@ void string_holder_free_last()
 
 /**
  * void construct(void)
- * 
+ *
  * Constructor volany pred main()
  * inicializuje string_holder
  * */
@@ -123,7 +123,7 @@ void __attribute__((constructor)) construct(void) // @ignore
 
 /**
  * void destructor(void)
- * 
+ *
  * Destructor volany po ukonceni main()
  * uvolni string_holder
  * */
@@ -135,7 +135,7 @@ void __attribute__((destructor)) destructor(void) // @ignore
 
 /**
  * void conversion_error(const char *msg)
- * 
+ *
  * Funkcia volana pri konverzii zo string na "Number"
  * Vypíše aj const char *msg
  * */
@@ -150,8 +150,7 @@ void conversion_error(const char *msg)
 
 /**
  * string get_string(void)
- * 
- * vrati string co uzivatel zadal do stdin
+ *  * vrati string co uzivatel zadal do stdin
  * */
 #undef get_string
 string get_string(void)
@@ -189,9 +188,10 @@ string get_string(void)
     // malo by stacit strcpy, memcpy moze byt rychlejsie
     // TODO Vysetri rychlost strcpy, memcpy
     //      pre male ale aj velke data
-    //      napis vlastne copy, pretoze vyssie spomenute
-    //      moze byt optimalizovane pre velke data
-    //      a moze byt pre male data pomale
+    // UPDATE on speed
+    // Vyzera to tak, ze memcpy je rychlejsie
+    // pre male velkosti (do 30 znakov) char* poli,
+    // zakial strcpy je rychlejsie pre dhsie      
     memcpy(text, buffer, charCounter);
     // strcpy(text, buffer);
 
@@ -213,9 +213,9 @@ string get_string(void)
 /**
  * long long get_long_long(void)
  * 
- * Vrati long long konvertovany zo string
+ Vrati long long konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_long_long
 long long get_long_long(void)
 {
@@ -248,9 +248,9 @@ long long get_long_long(void)
 /**
  * unsigned long long get_ulong_long(void)
  * 
- * Vrati unsigned long long konvertovany zo string
+ Vrati unsigned long long konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_ulong_long
 unsigned long long get_ulong_long(void)
 {
@@ -287,9 +287,9 @@ unsigned long long get_ulong_long(void)
 /**
  * long get_long(void)
  * 
- * Vrati long konvertovany zo string
+ Vrati long konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_long
 long get_long(void)
 {
@@ -323,9 +323,9 @@ long get_long(void)
 /**
  * unsigned long get_ulong(void)
  * 
- * Vrati unsigned long konvertovany zo string
+ Vrati unsigned long konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_ulong
 unsigned long get_ulong(void)
 {
@@ -363,9 +363,9 @@ unsigned long get_ulong(void)
 /**
  * int get_int(void)
  * 
- * Vrati integer konvertovany zo string
+ Vrati integer konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_int
 int get_int(void)
 {
@@ -399,9 +399,9 @@ int get_int(void)
 /**
  * unsigned int get_int(void)
  * 
- * Vrati unsigned integer konvertovany zo string
+ Vrati unsigned integer konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_uint
 unsigned int get_uint(void)
 {
@@ -439,9 +439,9 @@ unsigned int get_uint(void)
 /**
  * short get_short(void)
  * 
- * Vrati short konvertovany zo string
+ Vrati short konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_short
 short get_short(void)
 {
@@ -474,9 +474,9 @@ short get_short(void)
 /**
  * unsigned short get_ushort(void)
  * 
- * Vrati unsigned short konvertovany zo string
+ Vrati unsigned short konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_ushort
 unsigned short get_ushort(void)
 {
@@ -514,9 +514,9 @@ unsigned short get_ushort(void)
 /**
  * char get_char(void)
  * 
- * Vrati char konvertovany zo string (prvy character)
+ Vrati char konvertovany zo string (prvy character)
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_char
 char get_char(void)
 {
@@ -529,9 +529,9 @@ char get_char(void)
 /**
  * float get_float(void)
  * 
- * Vrati float  konvertovany zo string
+ Vrati float  konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_float
 float get_float(void)
 {
@@ -565,9 +565,9 @@ float get_float(void)
 /**
  * double get_double(void)
  * 
- * Vrati double  konvertovany zo string
+ Vrati double  konvertovany zo string
  * Funkcia vo vnutri vola string get_string(void); 
- * */
+ */
 #undef get_double
 double get_double(void)
 {
